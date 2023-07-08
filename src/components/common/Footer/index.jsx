531176386b742a605ec72dd137a35fabfb1cbbf7
suman <sumansinha.ellenox@gmail.com>
2023-07-08 11:29:22 +0530

    feat: added responsive herosection

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -42,106 +42,108 @@ const Footer = () => {
     }
   };
   return (
-    <Stack height="100vh" overflow="hidden" position="relative" zIndex={999}>
-      <Stack direction="row" justifyContent={`center`} gap="48px">
-        {socialLinks.map((link, i) => (
-          <Link
-            target="_blank"
-            to={link.socialLink}
-            key={i}
-            style={{ textDecoration: "none" }}
-          >
-            <Stack
-              sx={{
-                border: `3px solid gray`,
-                p: 2,
-                width: "150px",
-                borderRadius: "100vmax",
-                color: "#808080",
-              }}
-              mt={2}
-              alignItems="center"
-              justifyContent={"center"}
-              component={motion.div}
-              whileHover={{ borderColor: "#9fe870", color: "#9fe870" }}
+    <footer>
+      <Stack height="100vh" overflow="hidden" position="relative" zIndex={999}>
+        <Stack direction="row" justifyContent={`center`} gap="48px">
+          {socialLinks.map((link, i) => (
+            <Link
+              target="_blank"
+              to={link.socialLink}
+              key={i}
+              style={{ textDecoration: "none" }}
             >
-              <HeadingText
-                variant="h5"
+              <Stack
+                sx={{
+                  border: `3px solid gray`,
+                  p: 2,
+                  width: "150px",
+                  borderRadius: "100vmax",
+                  color: "#808080",
+                }}
+                mt={2}
+                alignItems="center"
+                justifyContent={"center"}
                 component={motion.div}
-                sx={{ color: "inherit", lineHeight: "90%" }}
-                variants={marqueeVariants}
-                animate="animate"
-                textAlign="center"
+                whileHover={{ borderColor: "#9fe870", color: "#9fe870" }}
               >
-                {link.socialPlatform}
-              </HeadingText>
-            </Stack>
-          </Link>
-        ))}
-      </Stack>
-      <Stack alignItems="center" mt={{ xs: 5, xl: `128px` }}>
-        <FooterHeadingText
-          variant="h5"
-          textAlign={"center"}
-          sx={{ width: { md: "50%", xl: "30%", xs: "350px" }, color: "gray" }}
-        >
-          Got a question, project or want to work together on something? Feel
-          free to reach out.
-        </FooterHeadingText>
-        <Stack
-          direction="row"
-          borderRadius={6}
-          bgcolor="#000000"
-          mt={{ xs: 4, xl: 5 }}
-          p={"16px"}
-          gap="16px"
-          position="relative"
-          zIndex={2}
-        >
-          <Stack p={1} px={3} borderRadius={5} sx={{ bgcolor: "#3232345e" }}>
-            <FooterText
-              variant="h3"
-              textAlign={"center"}
-              sx={{ color: "#9fe870" }}
-            >
-              sinhasumank41@gmail.com
-            </FooterText>
-          </Stack>
-          <Stack
-            p={1}
-            borderRadius={5}
-            sx={{ bgcolor: "#3232345e", cursor: "pointer" }}
-            onClick={copyEmail}
+                <HeadingText
+                  variant="h5"
+                  component={motion.div}
+                  sx={{ color: "inherit", lineHeight: "90%" }}
+                  variants={marqueeVariants}
+                  animate="animate"
+                  textAlign="center"
+                >
+                  {link.socialPlatform}
+                </HeadingText>
+              </Stack>
+            </Link>
+          ))}
+        </Stack>
+        <Stack alignItems="center" mt={{ xs: 5, xl: `128px` }}>
+          <FooterHeadingText
+            variant="h5"
+            textAlign={"center"}
+            sx={{ width: { md: "50%", xl: "30%", xs: "350px" }, color: "gray" }}
           >
-            <AnimatePresence>
-              {copied ? (
-                <FooterText variant="h3"> ✅</FooterText>
-              ) : (
-                <FooterText variant="h3">📋</FooterText>
-              )}
-            </AnimatePresence>
+            Got a question, project or want to work together on something? Feel
+            free to reach out.
+          </FooterHeadingText>
+          <Stack
+            direction="row"
+            borderRadius={6}
+            bgcolor="#000000"
+            mt={{ xs: 4, xl: 5 }}
+            p={"16px"}
+            gap="16px"
+            position="relative"
+            zIndex={2}
+          >
+            <Stack p={1} px={3} borderRadius={5} sx={{ bgcolor: "#3232345e" }}>
+              <FooterText
+                variant="h3"
+                textAlign={"center"}
+                sx={{ color: "#9fe870" }}
+              >
+                sinhasumank41@gmail.com
+              </FooterText>
+            </Stack>
+            <Stack
+              p={1}
+              borderRadius={5}
+              sx={{ bgcolor: "#3232345e", cursor: "pointer" }}
+              onClick={copyEmail}
+            >
+              <AnimatePresence>
+                {copied ? (
+                  <FooterText variant="h3"> ✅</FooterText>
+                ) : (
+                  <FooterText variant="h3">📋</FooterText>
+                )}
+              </AnimatePresence>
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
 
-      <Stack
-        sx={{
-          position: "absolute",
-          whiteSpace: `nowrap`,
-          bottom: 0,
-        }}
-        component={motion.div}
-       >
-        <FooterMarqueeText
+        <Stack
+          sx={{
+            position: "absolute",
+            whiteSpace: `nowrap`,
+            bottom: 0,
+          }}
           component={motion.div}
-          sx={{ fontSize: "26vw", color: "#3233344f", lineHeight: "70%" }}
-          variants={marqueeVariants}
-          animate="animate"
         >
-          {new Array(100).fill(0).map((item) => `Let's talk.`)}
-        </FooterMarqueeText>
+          <FooterMarqueeText
+            component={motion.div}
+            sx={{ fontSize: "26vw", color: "#3233344f", lineHeight: "70%" }}
+            variants={marqueeVariants}
+            animate="animate"
+          >
+            {new Array(100).fill(0).map((item) => `Let's talk.`)}
+          </FooterMarqueeText>
+        </Stack>
       </Stack>
-    </Stack>
+    </footer>
   );
 };
 
--- src/components/common/phone/HeroSection/index.jsx
+++ src/components/common/phone/HeroSection/index.jsx
@@ -1,10 +1,11 @@
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Button, IconButton, Stack } from "@mui/material";
 import React, { useContext, useEffect } from "react";
-import { HeadingText, SemiboldText } from "../../../styles/fonts";
+import { HeadingText, RegularText, SemiboldText } from "../../../styles/fonts";
 import { motion, useAnimationControls } from "framer-motion";
 import { constants } from "../../../../constants/index";
 import AppContext from "../../../../context/AppContext";
 import Images from "../../../../assets";
+import { ArrowDownward } from "@mui/icons-material";
 const entryContainerVariant = {
   initial: {
     opacity: 0,
@@ -32,10 +33,20 @@ const HeroSectionPhone = () => {
   const headingTextOne = `FRONT-END`.split("");
 
   const headingTextTwo = `DEVELOPER`.split("");
-  const entryAnimaitonControl = useAnimationControls();
+  const entryAnimationControl = useAnimationControls();
+  const HeroControl = useAnimationControls();
   useEffect(() => {
     if (!loadingAnimationComplete) {
-      entryAnimaitonControl.start("animate");
+      entryAnimationControl.start("animate");
+      HeroControl.start((i) => ({
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: i * 0.1,
+          ...constants[`transitions`],
+        },
+      }));
     }
   }, [loadingAnimationComplete]);
   const renderNavbar = () => {
@@ -51,7 +62,7 @@ const HeroSectionPhone = () => {
           component={motion.div}
           variants={entryContainerVariant}
           initial="initial"
-          animate={entryAnimaitonControl}
+          animate={entryAnimationControl}
           overflow="hidden"
         >
           <Stack component={motion.div} variants={entryVariant}>
@@ -91,30 +102,25 @@ const HeroSectionPhone = () => {
   const renderTitle = ({ text, image }) => {
     if (text) {
       return (
-        <Stack direction="row">
+        <Stack
+          direction="row"
+          overflow={loadingAnimationComplete ? "visible" : "hidden"}
+        >
           {text.map((item, i) => {
             return (
               <motion.div
                 key={i}
-                onAnimationComplete={() => {
-                  setFirstAnimationOver(true);
-                }}
+                custom={i}
                 whileHover={{
                   scale: 1.02,
                   color: ["#ffffe3", "#d6fb41"],
                 }}
                 initial={{ opacity: 0, y: 400 }}
-                animate={{
-                  opacity: 1,
-                  y: 0,
-                  transition: {
-                    duration: 1,
-                    delay: i * 0.1,
-                    ...constants[`transitions`],
-                  },
-                }}
+                animate={HeroControl}
               >
-                <HeadingText sx={styles.titleStyle}>{item}</HeadingText>
+                <HeadingText sx={styles.titleStyle} component="h1">
+                  {item}
+                </HeadingText>
               </motion.div>
             );
           })}
@@ -123,33 +129,111 @@ const HeroSectionPhone = () => {
     }
     if (Image) {
       return (
-        <Box
-          component="img"
-          src={image}
-          sx={{
-            height: "200px",
-            bgcolor: "#9fe870",
-            mt: 1,
-            mb: 1.5,
-            objectFit: "cover",
-          }}
-        />
+        <motion.div initial={{ opacity: 0, y: 400 }} animate={HeroControl}>
+          <Box
+            component="img"
+            src={image}
+            alt="Suman Kumar Sinha"
+            sx={{
+              height: "200px",
+              bgcolor: "#9fe870",
+              mt: 1,
+              mb: 1.5,
+              objectFit: "cover",
+              width: "100%",
+            }}
+          />
+        </motion.div>
       );
     }
+  };
+  const renderDescription = () => {
+    return (
+      <Stack
+        gap="48px"
+        width={{ xs: "90% ", xl: "500px " }}
+        overflow="hidden"
+        mx={"auto"}
+        mt={"32px"}
+        initial="initial"
+        variants={entryVariant}
+        component={motion.div}
+        animate={entryAnimationControl}
+      >
+        <RegularText
+          sx={{
+            lineHeight: "120%",
+            fontSize: { xs: "18px", xl: "22px" },
+            textAlign: "center",
+          }}
+        >
+          <SemiboldText
+            component={"span"}
+            mr={"16px"}
+            sx={{
+              lineHeight: "120%",
+              color: "#777777",
+            }}
+            variant="h6"
+          >
+            ABOUT
+          </SemiboldText>
+          I am a developer based in Guwahati, India, with a specialization in
+          building exceptional digital experiences on the web. My current focus
+          revolves around crafting remarkable products at Ellenox.
+        </RegularText>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          component={motion.div}
+          variants={entryVariant}
+        >
+          <SemiboldText sx={{ color: "#777777" }} variant="h6">
+            Scroll Down
+          </SemiboldText>
+          <IconButton
+            sx={{
+              height: "40px",
+              width: "40px",
+              overflow: "hidden",
+            }}
+          >
+            <motion.div
+              initial={{ y: "-100%" }}
+              animate={{
+                y: ["-100%", "0%", "5%", "-5%", "100%"],
+                transition: {
+                  duration: 1.3,
+                  repeat: "infinity",
+                },
+              }}
+            >
+              <ArrowDownward sx={{ color: "#777777", fontSize: "30px" }} />
+            </motion.div>
+          </IconButton>
+        </Stack>
+      </Stack>
+    );
   };
   return (
     <Stack minHeight="100vh">
       {renderNavbar()}
-      <Stack
-        mx="auto"
-        justifyContent={"space-between"}
-        component={motion.div}
-        variants={entryVariant}
-      >
-        {renderTitle({ text: headingTextOne })}
-        {renderTitle({ image: Images.SumanImage })}
-        {renderTitle({ text: headingTextTwo })}
-      </Stack>
+
+      <section role="main">
+        <Stack
+          mx="auto"
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          component={motion.div}
+          variants={entryVariant}
+          sx={{ overflow: "hidden" }}
+        >
+          {renderTitle({ text: headingTextOne })}
+          {renderTitle({ image: Images.SumanImage })}
+          {renderTitle({ text: headingTextTwo })}
+        </Stack>
+        {renderDescription()}
+      </section>
     </Stack>
   );
 };
